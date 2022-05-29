36f410179e6d6ec21f93c037c07c3a6259d26843
Mark <mark.ammermueller@tu-dortmund.de>
2022-05-29 10:54:30 +0200

    Implement extractInstruments

--- AMUSE-2/amuse/tools/Teilprojekt2/tool/Teilprojekt2.java
+++ AMUSE-2/amuse/tools/Teilprojekt2/tool/Teilprojekt2.java
@@ -1,19 +1,32 @@
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class Teilprojekt2 {
     public static void main(String[] args) throws IOException {
-        String[] argss = new String[4];
+        /*String[] argss = new String[4];
         argss[0] = "[\"C:\\Users\\install\\OneDrive - OrgaTech Solution Engineering Consulting GmbH\\Desktop\\x2go2\\Genre";
         argss[1] = "rock";
         argss[2] = "5";
         argss[3] = "5";
-        ArffCreator.main(argss);
-        initData();
+        ArffCreator.main(argss);*/
+        initData();/*
         searchAllFiles();
         filterCriteria();
-        outputFile();
+        outputFile();*/
     }
-    public static void initData() {
+    public static void initData(){
+        String instrumentPfad = "AMUSE-2/amuse/tools/Teilprojekt2/assets/toolInput/Instruments.arff";//sollte aus config kommen
+        File instrumentsArff = new File(instrumentPfad);
+        try {
+            System.out.println(extractInstruments(instrumentsArff).toString());
+        }
+        catch(Exception e){
+
+        }
         /// Instrument.arff/SampleLists, XML-Datei(anzahlInstrumente/Noten) ->
         // TODO Auslesen aller Samples(instrument_Note.wav)
         //  -> File[] mit den Pfaden aller Samples
@@ -21,6 +34,25 @@ public class Teilprojekt2 {
         // -> File[] mit allesn Randomm ausgewählten Instrumenten
         //todo mithilfe von gausscher-Verteilung -> ArrayList Pfade zu allen Noten.wav Datein
         ///TODO AudioFileMixing -> wav Datei mit generierter Note
+    }
+
+    public static List<Instrument> extractInstruments(File instrumentsArff)throws IOException{
+        List<Instrument> extractedIntstruments = new ArrayList<Instrument>();
+        BufferedReader rd = new BufferedReader(new FileReader(instrumentsArff));
+        String line;
+        while((line = rd.readLine()) != null){
+            if(line.contains(",")){
+                int iID = Integer.parseInt(line.substring(0,line.indexOf(",")));
+                String iName = line.substring(1,line.indexOf(","));
+                int iStyleID = Integer.parseInt(line.substring(2,line.indexOf(",")));
+                String iPfad = line.substring(3,line.indexOf(","));
+                int iLowestPitch = Integer.parseInt(line.substring(4,line.indexOf(",")));
+                int iHighestPitch = Integer.parseInt(line.substring(5,line.indexOf(",")));
+                extractedIntstruments.add(new Instrument(iID, iName, iStyleID, iPfad, iLowestPitch, iHighestPitch));
+            }
+        }
+        rd.close();
+        return extractedIntstruments;
     }
 
     public static void selectSamples(int anzahlInstrumente, int anzahlNoten) {
