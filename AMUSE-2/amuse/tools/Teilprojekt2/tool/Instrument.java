324e2ebe2b5d831cdc1e727bb49b7938cec332eb
Mark <mark.ammermueller@tu-dortmund.de>
2022-05-31 14:06:28 +0200

    Fix Instruments (Add styleName)

--- AMUSE-2/amuse/tools/Teilprojekt2/tool/Instrument.java
+++ AMUSE-2/amuse/tools/Teilprojekt2/tool/Instrument.java
@@ -1,21 +1,28 @@
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Instrument {
     public int iD;
     public String name;
     public int styleID;
+    public String styleName;
     public String pfad;
     public int lowestPitch;
     public int highestPitch;
-    public Instrument(int piD, String pname, int pstyleID, String ppfad, int plowestPitch, int phighestPitch){
+    public ArrayList<String> Notes;
+    public Instrument(int piD, String pname, int pstyleID, String pstyleName, String ppfad, int plowestPitch, int phighestPitch){
         iD = piD;
         name = pname;
         styleID = pstyleID;
+        styleName = pstyleName
         pfad = ppfad;
         lowestPitch = plowestPitch;
         highestPitch = phighestPitch;
     }
     public String toString(){
-        return iD + ", " + name + ", " + styleID + ", " + pfad + ", " + lowestPitch + ", " + highestPitch;
+        return iD + ", " + name + ", " + styleID + ", " + styleName + ", " + pfad + ", " + lowestPitch + ", " + highestPitch;
     }
+
+    public void add
 }
